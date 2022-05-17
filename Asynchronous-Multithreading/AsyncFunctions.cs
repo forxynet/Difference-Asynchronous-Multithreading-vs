58579cb1e3ba7ef2d3146d9fc33597ad6f22750c
hakan baştürk <hakan.basturk@fuzulev.com>
2022-05-17 12:37:40 +0300

    first commit

---
+++ Asynchronous-Multithreading/AsyncFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.AsyncSample
+{
+    public class AsyncFunctions
+    {
+        public async Task FirstAsync() {
+            Console.WriteLine("First Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("First Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public async Task SecondAsync() {
+            Console.WriteLine("Second Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Second Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public async Task ThirdAsync() {
+            Console.WriteLine("Third Async Method on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(1000);
+            Console.WriteLine("Third Async Method Continuation on Thread with Id: " + Thread.CurrentThread.ManagedThreadId);
+        }
+        public async Task ExecuteAsyncFunctions() {
+            var firstAsync = FirstAsync();
+            var secondAsync = SecondAsync();
+            var thirdAsync = ThirdAsync();
+            await Task.WhenAll(firstAsync, secondAsync, thirdAsync);
+        }
+    }
+}
---
+++ Asynchronous-Multithreading/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp.AsyncSample;
+using System;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Multithreading
+{
+    internal class Program
+    {
+        static async Task Main(string[] args) {
+            AsyncFunctions asyncFunctions = new AsyncFunctions();
+            await asyncFunctions.ExecuteAsyncFunctions();
+
+            Console.WriteLine();
+
+            Multithreading multithreading = new Multithreading();
+            multithreading.ExecuteMultithreading();
+        }
+    }
+}
